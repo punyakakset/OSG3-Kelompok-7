1f1f9310c69bd13a8a67150889700ce8e5e7cb46
Muhammad Wahyudin <muhammad.whydn@gmail.com>
2019-03-21 14:16:35 +0700

    Init base project

---
+++ app/src/androidTest/java/osg3/eudeka/tujuh/app/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package osg3.eudeka.tujuh.app
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("osg3.eudeka.tujuh.app", appContext.packageName)
+    }
+}
