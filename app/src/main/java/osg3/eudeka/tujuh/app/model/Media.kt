bdfebbc410850ff877884dcf257ce540823136be
pendi-qibee <pendimadyana@gmail.com>
2019-04-02 09:58:45 +0700

    add data model

---
+++ app/src/main/java/osg3/eudeka/tujuh/app/model/Media.kt
@@ -0,0 +1,6 @@
+package osg3.eudeka.tujuh.app.model
+
+data class Media(
+	val imageUrl: Any? = null,
+	val imageUrlThumb: Any? = null
+)
---
+++ app/src/main/java/osg3/eudeka/tujuh/app/model/Source.kt
@@ -0,0 +1,7 @@
+package osg3.eudeka.tujuh.app.model
+
+data class Source(
+	val apiUrl: String? = null,
+	val htmlUrl: String? = null,
+	val name: String? = null
+)
