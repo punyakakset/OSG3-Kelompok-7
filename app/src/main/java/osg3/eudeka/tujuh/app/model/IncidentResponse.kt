4be07b09d24a2bcada5c72ca2950c403e161331a
pendi-qibee <pendimadyana@gmail.com>
2019-04-02 09:59:11 +0700

    add data model

---
+++ app/src/main/java/osg3/eudeka/tujuh/app/model/IncidentResponse.kt
@@ -0,0 +1,5 @@
+package osg3.eudeka.tujuh.app.model
+
+data class IncidentResponse(
+	val incidents: List<IncidentsItem?>? = null
+)
---
+++ app/src/main/java/osg3/eudeka/tujuh/app/model/IncidentsItem.kt
@@ -0,0 +1,17 @@
+package osg3.eudeka.tujuh.app.model
+
+data class IncidentsItem(
+	val address: Any? = null,
+	val description: Any? = null,
+	val source: Any? = null,
+	val media: Media? = null,
+	val title: Any? = null,
+	val locationDescription: Any? = null,
+	val type: String? = null,
+	val typeProperties: Any? = null,
+	val url: String? = null,
+	val locationType: Any? = null,
+	val occurredAt: Int? = null,
+	val updatedAt: Int? = null,
+	val id: Int? = null
+)
